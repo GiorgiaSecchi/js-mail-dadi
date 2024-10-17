d46fe73b767dfa7e2b1fd2e9ac0d86e80962cfe5
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-17 17:27:12 +0200

    elaborazione

--- 2 -dadi/gioco-dadi.js
+++ 2 -dadi/gioco-dadi.js
@@ -7,14 +7,32 @@
 // genera numero random computer
 
 //! ELABORAZIONE
-//
+// SE numero giocatore è più grande
+//  messaggio vincitore giocatore
+//  ALTRIMENTI SE numero pc più grande
+//  messaggio vincitore pc
 
 //! OUTPUT
+// messaggio di chi avrà vinto
 
 // **************************************************************
 
 const playerNumb = Math.floor(Math.random() * 7);
-console.log("Player:" + playerNumb);
+console.log("Giocatore:" + " " + playerNumb);
 
 const computerNumb = Math.floor(Math.random() * 7);
-console.log("Computer:" + computerNumb);
+console.log("Computer:" + " " + computerNumb);
+
+let outputMsg = "";
+
+if (playerNumb > computerNumb) {
+  outputMsg = "Giocatore è il vincitore! :)";
+} else {
+  outputMsg = "Computer è il vincitore! :)";
+}
+
+if (playerNumb === computerNumb) {
+  outputMsg = "Parità... ritenta ancora!";
+}
+
+console.log(outputMsg);
