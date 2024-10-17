8bcedf2b552c8d3f2610303591e76c9d9c2d38ed
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-17 15:25:42 +0200

    pseudocodice

--- js/mail.js
+++ js/mail.js
@@ -2,3 +2,22 @@
 // Chiedi all'utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull'esito del controllo.
+
+//! RACCOLTA DATI
+// chiedere una email
+
+//! ELABORAZIONE
+// controllare all'interno di una lista (array) l'email inserita
+// SE email inserita è valida
+//      PER OGNI (for) email dentro la lista (array)
+//          verificare se si trova corrispondenza con la email utente
+//          SE trova corrispondenza
+//          messaggio riscontro positivo
+//          ALTRIMENTI
+//          messaggio riscontro negativo
+//
+
+//! OUTPUT
+// stampare un messaggio a seconda del risultato
+
+// *****************************************************************************
